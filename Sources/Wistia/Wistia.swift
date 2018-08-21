355cbe91bfa1231075246df6b9bbba7ff4ac75b8
Jake Young <jake@acpdecisions.org>
2018-08-21 10:26:10 -0400

    Adds errors and models

--- Sources/Wistia/Wistia.swift
+++ Sources/Wistia/Wistia.swift
@@ -1,3 +1,82 @@
-struct Wistia {
-    var text = "Hello, World!"
+import Foundation
+
+/// Wistia.
+/// Class for managing common Wistia Data API Tasks.
+public class Wistia {
+    
+    // MARK: Properties
+    let baseURL = "https://api.wistia.com/v1/"
+    let api_password: String
+    let session = URLSession.shared
+    
+    init(api_password: String) {
+        self.api_password = api_password
+    }
+    
+}
+
+// MARK: - Error Handling
+extension Wistia {
+    
+    enum WistiaError: Error, CustomStringConvertible {
+        case invalidAPIKey
+        case noData
+        var description: String {
+            switch self {
+                
+            case .invalidAPIKey:
+                return "An invalid API key has been provided."
+            case .noData:
+                return "No data was provided from the Wistia API."
+            }
+        }
+    }
+    
+}
+
+// MARK: - Models
+extension Wistia {
+    
+    // MARK: - Projects
+    struct Project: Codable {
+        let hashed_id: String
+        let medias: [Media]
+    }
+    
+    // MARK: - Medias
+    struct Media: Codable {
+        
+        let hashed_id: String
+        let name: String
+        let type: String
+        let description: String
+        let section: String?
+        
+        let thumbnail: Thumbnail
+        
+        let assets: [Asset]
+        
+        struct Thumbnail: Codable {
+            let url: URL
+            let width: Int
+            let height: Int
+        }
+        
+        struct Caption: Codable {
+            let english_name: String
+            let native_name: String
+            let language: String
+            let text: String
+        }
+        
+        struct Asset: Codable {
+            let url: URL
+            let contentType: String
+            let type: String
+            let fileSize: Int
+            let height: Int
+            let width: Int
+        }
+    }
+        
 }
